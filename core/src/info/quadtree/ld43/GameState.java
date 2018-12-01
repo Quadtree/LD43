6ef1b8e05b90c28417a15e23a11da9aea329e3c5
Quadtree <quadtree@gmail.com>
2018-11-30 23:28:49 -0800

    Updated

--- core/src/info/quadtree/ld43/GameState.java
+++ core/src/info/quadtree/ld43/GameState.java
@@ -14,6 +14,19 @@ public class GameState {
 
         pc = new Creature();
         pc.graphicName = "pc1";
+
+        if (Util.randInt(3) == 0) pc.statMagic = Util.randInt(40);
+
+        while(true){
+            switch(Util.randInt(3)){
+                case 0: pc.statPower++; break;
+                case 1: pc.statSpeed++; break;
+                case 2: pc.statEndurance++; break;
+            }
+
+            if (pc.statPower + pc.statSpeed + pc.statEndurance + pc.statMagic >= 80) break;
+        }
+
         pc.pos = TilePos.create(WorldMap.WORLD_WIDTH / 2, 1);
 
         creatures.add(pc);
