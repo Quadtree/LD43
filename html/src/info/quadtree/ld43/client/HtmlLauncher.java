cc58a5bf3002436d04f14548f6ee1677e1d89868
Quadtree <quadtree@gmail.com>
2018-12-01 15:17:48 -0800

    HTML5 version now resizes

--- html/src/info/quadtree/ld43/client/HtmlLauncher.java
+++ html/src/info/quadtree/ld43/client/HtmlLauncher.java
@@ -3,13 +3,14 @@ package info.quadtree.ld43.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.google.gwt.user.client.Window;
 import info.quadtree.ld43.LD43;
 
 public class HtmlLauncher extends GwtApplication {
 
         @Override
         public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
+                return new GwtApplicationConfiguration(Window.getClientWidth(), Window.getClientHeight());
         }
 
         @Override
