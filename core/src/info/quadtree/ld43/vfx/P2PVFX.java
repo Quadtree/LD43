0f452f8f3b5f6ca1cdeafaec84769eb6a6a0362c
Quadtree <quadtree@gmail.com>
2018-12-02 01:23:13 -0800

    Updated

--- core/src/info/quadtree/ld43/vfx/P2PVFX.java
+++ core/src/info/quadtree/ld43/vfx/P2PVFX.java
@@ -1,6 +1,35 @@
 package info.quadtree.ld43.vfx;
 
+import com.badlogic.gdx.math.Vector2;
+import info.quadtree.ld43.TilePos;
 import info.quadtree.ld43.action.BaseAction;
 
 public class P2PVFX extends BaseVisualEffect {
+    TilePos start;
+    TilePos end;
+    String graphic;
+
+    Vector2 curPos;
+
+    float rangeToGo;
+
+    public P2PVFX(TilePos start, TilePos end, String graphic) {
+        this.start = start;
+        this.end = end;
+        this.graphic = graphic;
+
+        rangeToGo = (float)Math.sqrt(Math.pow(start.x - end.x, 2)+Math.pow(start.y - end.y, 2));
+
+        curPos = new Vector2(start.x, start.y);
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public boolean keep() {
+        return rangeToGo > 0;
+    }
 }
